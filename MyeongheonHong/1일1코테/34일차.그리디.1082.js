23726677e7195713e15ef1df25196d56302f7ab4
myeongheonhong <ruddyh7076@gmail.com>
2025-01-13 19:53:17 +0900

    [ 1일1코테 ] 34일차 - 1차도전코드(실패)

--- MyeongheonHong/1일1코테/34일차.그리디.1082.js
+++ MyeongheonHong/1일1코테/34일차.그리디.1082.js
@@ -6,31 +6,51 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
 const input = fs.readFileSync(filePath).toString().trim().split('\n')
 const N = +input.shift()
 const priceArray = input.shift().split(' ').map(Number)
+const price = []
+const share = []
 let limit = +input.shift()
-const map = new Map()
+const maxList = []
+let max = ''
 
 for (let i = 0; i < N; i++) {
-  map.set(i, priceArray[i])
+  price.push([i, priceArray[i]])
+  share.push([i, Math.floor(limit / priceArray[i])])
 }
 
-function solution() {
-  const answer = []
+share.sort((a, b) => b[1] - a[1] || b[0] - a[0])
+price.sort((a, b) => b[1] - a[1] || b[0] - a[0])
 
-  for (let i = N; i > 0; i--) {
-    const priceN = []
+for (let i = 0; i < share[0][1]; i++) {
+  max += share[0][0].toString()
+}
 
-    map.forEach((value, key) => {
-      if (N - i + 1 === value) {
-        priceN.push(key)
-      }
-    })
+maxList.push(max)
 
-    priceN.sort((a, b) => b - a)
+let sum = 0
+max = ''
 
-    if (priceN.length > 0 && priceN[0] !== 0) {
-      answer.push(priceN.toString() * Math.floor(limit / (N - i + 1)))
-    }
+for (let i = 0; i < N; i++) {
+  const [key, value] = price[i]
+  if (value > limit) continue
+
+  limit -= value
+
+  if (limit < 0) {
+    limit += value
+    break
   }
+
+  max += key.toString()
 }
 
-solution()
+if (limit >= price[price.length - 1][1]) {
+  max += '0'
+}
+
+maxList.push(max)
+const answer = maxList.map(Number)
+
+console.log(Math.max(...answer).toString())
+
+// 1. 낮은 가격으로 정렬
+// 2. 같은 가격이라면 숫자가 높은 순으로 정렬
