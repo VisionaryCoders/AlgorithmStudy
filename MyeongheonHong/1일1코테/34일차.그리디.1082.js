bf027e916a9628623ec1a2aed2555eb9c14bf47d
myeongheonhong <ruddyh7076@gmail.com>
2025-01-13 21:47:35 +0900

    [ 1일1코테 ] 34일차 - 그리디(골드3) ** 실패 로직적으로 예제만 맞게 나오는거라 반례를 적용했을 때 틀림..

--- MyeongheonHong/1일1코테/34일차.그리디.1082.js
+++ MyeongheonHong/1일1코테/34일차.그리디.1082.js
@@ -8,13 +8,13 @@ const N = +input.shift()
 const priceArray = input.shift().split(' ').map(Number)
 const price = []
 const share = []
-let limit = +input.shift()
+let M = +input.shift()
 const maxList = []
 let max = ''
 
 for (let i = 0; i < N; i++) {
   price.push([i, priceArray[i]])
-  share.push([i, Math.floor(limit / priceArray[i])])
+  share.push([i, Math.floor(M / priceArray[i])])
 }
 
 share.sort((a, b) => b[1] - a[1] || b[0] - a[0])
@@ -24,33 +24,62 @@ for (let i = 0; i < share[0][1]; i++) {
   max += share[0][0].toString()
 }
 
-maxList.push(max)
+if (max.length === M && max[0] === '0') {
+  const cheap = [...price]
+  cheap.sort((a, b) => a[1] - b[1] || b[1] - a[1])
 
-let sum = 0
-max = ''
+  let cutIndex = 0
 
-for (let i = 0; i < N; i++) {
-  const [key, value] = price[i]
-  if (value > limit) continue
-
-  limit -= value
-
-  if (limit < 0) {
-    limit += value
-    break
+  if (cheap[0][0] === 0) {
+    cutIndex = cheap[1][1]
+    max = cheap[1][0].toString() + max
+  } else {
+    cutIndex = cheap[0][1]
+    max = cheap[0][0].toString() + max
   }
 
-  max += key.toString()
-}
-
-if (limit >= price[price.length - 1][1]) {
-  max += '0'
+  max = max.slice(0, -cutIndex)
 }
 
 maxList.push(max)
-const answer = maxList.map(Number)
+// console.log(price)
 
-console.log(Math.max(...answer).toString())
+for (let i = 0; i < N; i++) {
+  max = ''
+  const [key, value] = price[i]
+  let index = 0
+  let limit = M
+
+  if (value > limit) continue
+
+  while (limit > 0 && index < N) {
+    const value = price[index][1]
+
+    // console.log('value : ', value, limit)
+
+    limit -= value
+
+    // console.log('after limit : ', limit)
+
+    if (limit < 0) {
+      limit += value
+      index++
+    } else {
+      max += price[index++][0].toString()
+    }
+  }
+
+  if (limit >= price[price.length - 1][1]) {
+    max += '0'
+  }
+
+  maxList.push(max)
+}
+
+maxList.push(max)
+
+const answer = maxList.map(BigInt)
+console.log(answer.reduce((a, b) => (a > b ? a : b)).toString())
 
 // 1. 낮은 가격으로 정렬
 // 2. 같은 가격이라면 숫자가 높은 순으로 정렬
